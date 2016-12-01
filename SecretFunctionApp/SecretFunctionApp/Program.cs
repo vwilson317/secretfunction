80038e28920d04926b6584ea62916d662adbcd00
Vincent Wilson <vwilson@cardlytics.com>
2016-11-30 22:09:51 -0500

    project setup

---
+++ SecretFunctionApp/SecretFunctionApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecretFunctionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input an Int");
+            int? input = null;
+            try
+            {
+                input = Int16.Parse(Console.ReadLine());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Invaild input");
+                Console.WriteLine(e.Message);
+                Console.ReadLine();
+            }
+
+            if (input != null)
+            {
+                
+            }
+        }
+    }
+}
